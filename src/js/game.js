2f1afae5734533526fb0e74735a676bfad06f8e4
luismigm <luismigm@hotmail.com>
2014-03-11 10:19:47 -0400

    snake con lista enlazada

--- src/js/game.js
+++ src/js/game.js
@@ -7,8 +7,8 @@
     this.numberRows = 29;
     this.numberColumn = 39;
     this.floor = null;
-    this.snakeHead = new Array(10,10);//posiciones x, y de la cabeza
-    this.snakeTail = new Array(8,10);//posiciones x, y de la cola
+    this.snakeSprite = null;
+    this.snake = new Array(1);
     this.upKey = null;
     this.downKey = null;
     this.leftKey = null;
@@ -20,13 +20,11 @@
     create: function () {
       var x = this.game.width / 2
         , y = this.game.height / 2;
+      
+      this.background = this.add.sprite(0, 0, 'background');
 
-        this.background = this.add.sprite(0, 0, 'background');
-
-
-      //creamos el suelo
-      this.createFloor();
       this.initSnake();
+      //console.log(firstNode.x);
 
       //keys asignement 
     this.upKey = this.input.keyboard.addKey(Phaser.Keyboard.UP);
@@ -34,17 +32,14 @@
     this.leftKey = this.input.keyboard.addKey(Phaser.Keyboard.LEFT);
     this.rightKey = this.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
     this.spaceBar = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-
     },
 
     update: function () {
       
         if (this.upKey.isDown)
       {
-
-          
-          }
+          this.newSnakeNode(this.snake.lenght-1, 0, 1);
+      }
          
       if (this.downKey.isDown)
       {
@@ -64,32 +59,35 @@
     onInputDown: function () {
       //this.game.state.start('menu');
     },
-    createFloor: function ()
+    /*createFloor: function ()
     {
       this.floor = new Array(this.numberRows);
       for (var i = 0; i < this.numberRows; i++) 
       {
         this.floor[i] = new Array(this.numberColumn);
       }
-      for (var i = 0; i < this.numberRows; i++)
-      {
-        for (var j = 0; j < this.numberColumn; j++)
-        {
-          this.floor[i][j] = -1;
-        }
-      }
-    },
+      
+    },*/
     initSnake: function ()
     {
-        for (var i = 8; i <= 10; i++ )
-        {
-          this.floor[i][10] = this.add.sprite( ((20*i) + 10), (20*10), 'snake_body');
-          //console.log("hola");
-
-        }
-
-        this.add.sprite( ((20*10) + 10), (20*10), 'snake_head');
-
+       this.snake [0] = new this.SnakeNode( 9, 9, null, null, this.add.sprite( ((20 * 9) + 10), ((20 * 9) + 10), 'snake_head'));
+       this.newSnakeNode(this.snake[0], 1, 0);
+    },
+    newSnakeNode: function (last, x, y)
+    {
+       //last.sprite.destroy();
+       last.sprite = this.add.sprite( (((last.x )* 20) + 10), (((last.y )* 20) + 10), 'snake_body');
+       last.xPrevious += x;
+       last.yPrevious += y;
+       this.snake.push(new this.SnakeNode( last.x + x, last.y + y , null , null , this.add.sprite( (((last.x + x )* 20) + 10), (((last.y + y)* 20) + 10), 'snake_head')));
+    },
+    SnakeNode: function (x, y, xPrevious, yPrevious, sprite) 
+    { 
+      this.x = x; 
+      this.y = y;
+      this.xPrevious = xPrevious;
+      this.yPrevious = yPrevious;
+      this.sprite = sprite; 
     },
 
   };

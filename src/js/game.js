f515d547b5a708bb30303788e544774f7ec849b1
luismigm <luismigm@hotmail.com>
2014-03-16 16:42:12 -0400

    colisiones y salir del cuadro

--- src/js/game.js
+++ src/js/game.js
@@ -6,8 +6,6 @@
     this.score = null;
     this.scoreText = null;
     this.background = null;
-    this.numberRows = 29;
-    this.numberColumn = 39;
     this.floor = null;
     this.snakeSprite = null;
     this.snake = [];
@@ -23,21 +21,14 @@
   Game.prototype = {
 
     create: function () {
-      var x = this.game.width / 2
-        , y = this.game.height / 2;
-         this.time = this.game.time.now;
-      
+
+      this.time = this.game.time.now;
       this.background = this.add.sprite(0, 0, 'background');
-
-
       this.food = this.add.sprite( 10 + (Math.floor((Math.random()*780)+1)), 10 + (Math.floor((Math.random()*600)+1)), 'egg_green');
-
       this.scoreText = this.add.text(20, 20, 'SCORE: 0', { font: "20px Arial", fill: "#16DE16", align: "left" });
-
       this.initSnake();
-      //console.log(firstNode.x);
 
-      //keys asignement 
+      //asignación de teclas
     this.upKey = this.input.keyboard.addKey(Phaser.Keyboard.UP);
     this.downKey = this.input.keyboard.addKey(Phaser.Keyboard.DOWN);
     this.leftKey = this.input.keyboard.addKey(Phaser.Keyboard.LEFT);
@@ -49,32 +40,12 @@
 
       if(this.time +100 < this.game.time.now)
       {
-        if(this.direction === 0)
-        {
-          this.newSnakeNode(this.snake[(this.snake.length)-1], 0, -1);
-          this.deleteNode();
-        }
-        if(this.direction === 1)
-        {
-          this.newSnakeNode(this.snake[(this.snake.length)-1], 1, 0);
-          this.deleteNode();
-        }
-        if(this.direction === 2)
-        {
-          this.newSnakeNode(this.snake[(this.snake.length)-1], 0, 1);
-          this.deleteNode();
-        }
-        if(this.direction === 3)
-        {
-          this.newSnakeNode(this.snake[(this.snake.length)-1], -1, 0);
-          this.deleteNode();
-        }
+        this.createNewNode();
+        this.deleteNode();
         this.time = this.game.time.now;
         
       }
-
-      
-        if (this.upKey.isDown)
+      if (this.upKey.isDown)
       {
             this.direction = 0;
       }
@@ -92,33 +63,63 @@
             this.direction = 1;
       }
 
+      //comer
+      this.physics.overlap(this.snake[(this.snake.length)-1].sprite, this.food, function (snake_head, food) 
+      {  
+        food.kill(); this.score += 10;
+        this.scoreText.content = 'SCORE: ' + this.score;
 
-      this.physics.overlap(this.snake_head, this.food, function (snake_head, food) {  food.kill(); this.score += 10; this.scoreText.content = 'SCORE: ' + this.score;}, null, this);
-     
-    },
+        this.createNewNode();
+        this.food = this.add.sprite( 20 + (Math.floor((Math.random()*740))), 20 + (Math.floor((Math.random()*560))), 'egg_green');
 
-    onInputDown: function () {
-      //this.game.state.start('menu');
-    },
-    /*createFloor: function ()
-    {
-      this.floor = new Array(this.numberRows);
-      for (var i = 0; i < this.numberRows; i++) 
+      }, null, this);
+
+      //perder partida por chocar con el cuerpo de la serpiente
+      for (var i = this.snake.length -3; i > 0; i--)
       {
-        this.floor[i] = new Array(this.numberColumn);
+        this.physics.overlap(this.snake[(this.snake.length)-1].sprite, this.snake[i].sprite, function (snake_head, snake) 
+        {  
+          snake.kill(); this.score += 10;
+          this.scoreText.content = 'Has perdido: ' + this.score;
+        }, null, this);
       }
-      
-    },*/
+
+      //perder partida por salirte del borde.
+      if ((this.snake[(this.snake.length)-1].x < 1) || (this.snake[(this.snake.length)-1].x > 38) ||
+         (this.snake[(this.snake.length)-1].y < 1) || (this.snake[(this.snake.length)-1].y >  28) )
+      {
+          this.scoreText.content = 'Te has salido: ' + this.snake[(this.snake.length)-1].x + ' ' +this.snake[(this.snake.length)-1].y ;
+      }
+    },
+    createNewNode: function ()
+    {
+        if(this.direction === 0)
+        {
+          this.newSnakeNode(this.snake[(this.snake.length)-1], 0, -1);
+        }
+        if(this.direction === 1)
+        {
+          this.newSnakeNode(this.snake[(this.snake.length)-1], 1, 0);
+        }
+        if(this.direction === 2)
+        {
+          this.newSnakeNode(this.snake[(this.snake.length)-1], 0, 1);
+        }
+        if(this.direction === 3)
+        {
+          this.newSnakeNode(this.snake[(this.snake.length)-1], -1, 0);
+        }
+    },
     initSnake: function ()
     {
-       this.size = this.snake.push(new this.SnakeNode( 9, 9, null, this.add.sprite( ((20 * 9) + 10), ((20 * 9) + 10), 'snake_head')));
+        this.snake.push(new this.SnakeNode( 9, 9, null, this.add.sprite( ((20 * 9) + 10), ((20 * 9) + 10), 'snake_head')));
        this.newSnakeNode(this.snake[0], 1, 0);
     },
     newSnakeNode: function (last, x, y)
     {
        last.sprite.destroy();
        last.sprite = this.add.sprite( (((last.x )* 20) + 10), (((last.y )* 20) + 10), 'snake_body');
-       this.size = this.snake.push(new this.SnakeNode( last.x + x, last.y + y , null ,  this.add.sprite( (((last.x + x )* 20) + 10), (((last.y + y)* 20) + 10), 'snake_head')));
+       this.snake.push(new this.SnakeNode( last.x + x, last.y + y , null ,  this.add.sprite( (((last.x + x )* 20) + 10), (((last.y + y)* 20) + 10), 'snake_head')));
 
     },
     SnakeNode: function (x, y, previous, sprite) 
